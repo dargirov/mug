abbf1d9aa930fc7cedf2a9e750a7e877d3842d59
Delcho Argirov <argirov@outlook.com>
2016-08-04 13:44:02 +0300

    added sitemap

--- Source/Web/MugStore.Web/Controllers/HomeController.cs
+++ Source/Web/MugStore.Web/Controllers/HomeController.cs
@@ -1,13 +1,20 @@
 ﻿namespace MugStore.Web.Controllers
 {
+    using System;
     using System.Configuration;
+    using System.Collections.Generic;
+    using System.Globalization;
     using System.Linq;
     using System.Net;
     using System.Net.Mail;
+    using System.Net.Mime;
+    using System.Text;
     using System.Web.Mvc;
+    using System.Xml.Linq;
     using Services.Data;
     using ViewModels.Home;
 
+    [RoutePrefix("")]
     public class HomeController : BaseController
     {
         private readonly IProductsService products;
@@ -89,6 +96,67 @@
         public ActionResult ImageHelp()
         {
             return this.View();
+        }
+
+        [Route("sitemap.xml")]
+        public ActionResult SitemapXml()
+        {
+            var nodes = new List<SitemapNode>();
+
+            nodes.Add(new SitemapNode() { Priority = 1, Url = Url.RouteUrl("Default", new { action = "Index" }, this.Request.Url.Scheme) });
+            nodes.Add(new SitemapNode() { Priority = 0.5, Url = Url.RouteUrl("Default", new { action = "Contacts" }, this.Request.Url.Scheme) });
+            nodes.Add(new SitemapNode() { Priority = 0.6, Url = Url.RouteUrl("Default", new { controller = "Gallery", action = "Index" }, this.Request.Url.Scheme) });
+
+            var products = this.products.Get().Where(p => p.Active).OrderByDescending(p => p.Id).ToList();
+            foreach (var product in products)
+            {
+                nodes.Add(new SitemapNode()
+                    {
+                        Priority = 0.8,
+                        Frequency = SitemapFrequency.Monthly,
+                        Url = Url.RouteUrl("Product", new { acronym = product.Acronym }, this.Request.Url.Scheme)
+                    });
+            }
+
+            var categories = this.categories.Get().OrderBy(c => c.Order).ToList();
+            foreach (var category in categories)
+            {
+                nodes.Add(new SitemapNode()
+                {
+                    Priority = 0.6,
+                    Frequency = SitemapFrequency.Weekly,
+                    Url = Url.RouteUrl("GalleryCategory", new { acronym = category.Acronym }, this.Request.Url.Scheme)
+                });
+            }
+
+            var xml = this.GetSitemapDocument(nodes);
+            return this.Content(xml, "application/xml", Encoding.UTF8);
+        }
+
+        private string GetSitemapDocument(IEnumerable<SitemapNode> sitemapNodes)
+        {
+            XNamespace xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9";
+            XElement root = new XElement(xmlns + "urlset");
+
+            foreach (SitemapNode sitemapNode in sitemapNodes)
+            {
+                XElement urlElement = new XElement(
+                    xmlns + "url",
+                    new XElement(xmlns + "loc", sitemapNode.Url),
+                    sitemapNode.LastModified == null ? null : new XElement(
+                        xmlns + "lastmod",
+                        sitemapNode.LastModified.Value.ToLocalTime().ToString("yyyy-MM-ddTHH:mm:sszzz")),
+                    sitemapNode.Frequency == null ? null : new XElement(
+                        xmlns + "changefreq",
+                        sitemapNode.Frequency.Value.ToString().ToLowerInvariant()),
+                    sitemapNode.Priority == null ? null : new XElement(
+                        xmlns + "priority",
+                        sitemapNode.Priority.Value.ToString("F1", CultureInfo.InvariantCulture)));
+                root.Add(urlElement);
+            }
+
+            XDocument document = new XDocument(root);
+            return document.ToString();
         }
     }
 }
